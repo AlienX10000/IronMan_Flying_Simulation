a0cc880a4f4f4efbd7eb4c5092a8079f9f79c50c
AlienX10000 <71133819+AlienX10000@users.noreply.github.com>
2021-01-22 18:46:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,134 @@
+var PLAY=1;
+var END=0;
+var gameState=PLAY;
+
+var ironMan, background, ironManImg, ironManImg2, backgroundImg, collide1, collide2, collide3, obj, objImg, objGroup;
+
+function preload() {
+  backgroundImg = loadImage("images/city_background.png");
+
+  ironManImg = loadImage("images/iron-man-flying.png");
+  ironManImg2 = loadAnimation("images/iron-man-hover.png");
+
+  objImg = loadImage("images/birdObj.png");
+}
+
+function setup() {
+  createCanvas(1000, 800);
+  
+  collide1 = createSprite(4000, -10, 10000, 20);
+  collide1.visible=false;
+  collide2 = createSprite(390, 400, 20, 1000);
+  collide2.visible=false;
+  collide3 = createSprite(4000, 600, 10000, 20);
+  collide3.visible=false;
+
+  background = createSprite(4090,400)
+  background.addImage(backgroundImg);
+
+  ironMan = createSprite(400,300,10,10);
+  ironMan.addImage("fly" ,ironManImg);
+  ironMan.scale=0.5;
+
+  objGroup = new Group()
+}
+
+function draw() {  
+  if (gameState === PLAY) {
+    bird();
+    
+    movement();
+
+    gameOver();
+
+    ironMan.velocityX = 10;
+
+  } else if (gameState === END) {
+    ironMan.velocityX = 0;
+    ironMan.velocityY = 0;
+
+    obj.velocityX = 0;
+    objGroup.lifetime = -1;
+
+  }
+  
+  camera.position.x = ironMan.x + 200;
+  camera.position.y = 400;
+  
+  if (ironMan.x >= 7400){
+    ironMan.x=400;
+  }
+  
+  collideEdges();  
+
+  drawSprites();
+
+  if (gameState === END) {
+    fill(rgb(255, 0, 0));
+    textSize(100);
+    stroke("black");
+    strokeWeight(5);
+    text("Simulation Over", ironMan.x-145, 350);
+
+    fill(rgb(255, 0, 0));
+    textSize(30);
+    stroke("black");
+    strokeWeight(3);
+    text("you hit a bird", ironMan.x+145, 400);
+    text("Press ctrl + r to play again", ironMan.x+60, 30);
+  }
+}
+
+function movement() {
+  if (keyDown("up")) {
+    ironMan.velocityY = -10;
+  }
+  if (keyDown("down")) {
+    ironMan.velocityY = +10;
+  }
+
+  if (keyWentUp("up")) {
+    ironMan.velocityY = 0;
+  }
+  if (keyWentUp("down")) {
+    ironMan.velocityY = 0;
+  }
+}
+
+function bird() {
+  if (World.frameCount % 100 === 0) {
+    objY = random(10, 600);
+    obj = createSprite(ironMan.x + 800, objY,10,10);
+    obj.addImage(objImg);
+    obj.scale = 0.15;
+    obj.velocityX = -7;
+    objGroup.lifetime = 70;
+    objGroup.add(obj)
+  }
+}
+
+function collideEdges() {
+  ironMan.collide(collide1);
+  ironMan.collide(collide2);
+  ironMan.collide(collide3);
+}
+
+function gameOver() {
+  if (ironMan.isTouching(objGroup)) {
+    gameState=END
+  }
+}
+
+function writeText() {
+  fill(rgb(255, 0, 0));
+  textSize(50);
+  stroke("black");
+  strokeWeight(5);
+  text("Simulation Over", ironMan.x+75, 350);
+
+  fill(255);
+  textSize(30);
+  stroke(rgb(22, 166, 164));
+  strokeWeight(4);
+  text("you hit a bird", ironMan.x+165, 400);
+}
